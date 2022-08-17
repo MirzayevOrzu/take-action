6f60364a5d44e36b27a4f2421edb21d0878b4b8d
MirzayevOrzu <mirzayevorzu127@gmail.com>
2022-08-17 21:00:23 +0500

    fix(actions): add 'many' to create many route

--- src/modules/actions/actions.controller.ts
+++ src/modules/actions/actions.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    ParseIntPipe,
+    Post,
+    Query,
+    Req,
+    UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ActionsService } from './actions.service';
 
@@ -7,8 +16,14 @@ export class ActionsController {
     constructor(private readonly actionsService: ActionsService) {}
 
     @UseGuards(JwtAuthGuard)
-    @Post()
+    @Post('many')
     createMany(@Req() req, @Body() payload) {
         return this.actionsService.createMany(payload, req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    list(@Req() req, @Query('playlist_id', ParseIntPipe) playlist_id) {
+        return this.actionsService.list({ playlist_id, user_id: req.user.id });
     }
 }
