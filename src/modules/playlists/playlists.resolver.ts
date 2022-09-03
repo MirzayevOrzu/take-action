fef9feb4740f83e0937ae273920600298f12cb13
MirzayevOrzu <mirzayevorzu127@gmail.com>
2022-09-03 17:40:32 +0500

    feat: playlists graphql crud

--- src/modules/playlists/playlists.resolver.ts
+++ src/modules/playlists/playlists.resolver.ts
@@ -1,5 +1,10 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CurrentUser } from 'src/common/decorators/current-user';
+import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { NewPlaylistInput } from './dto/new-playlist.input';
 import { PlaylistsArgs } from './dto/playlists.args';
+import { UpdatePlaylistInput } from './dto/update-playlist.input';
 import { Playlist } from './models/playlist.model';
 import { PlaylistsService } from './playlists.service';
 
@@ -7,8 +12,53 @@ import { PlaylistsService } from './playlists.service';
 export class PlaylistsResolver {
     constructor(private readonly playlistsService: PlaylistsService) {}
 
+    @UseGuards(GqlAuthGuard)
     @Query(() => [Playlist])
-    playlists(@Args() playlistsArgs: PlaylistsArgs): Promise<Playlist[]> {
-        return this.playlistsService.list(playlistsArgs);
+    playlists(
+        @Args() playlistsArgs: PlaylistsArgs,
+        @CurrentUser() user,
+    ): Promise<Playlist[]> {
+        return this.playlistsService.list({
+            ...playlistsArgs,
+            user_id: user.id,
+        });
+    }
+
+    @UseGuards(GqlAuthGuard)
+    @Query(() => Playlist)
+    playlist(@Args('id') id: number, @CurrentUser() user) {
+        return this.playlistsService.show({ id, user_id: user.id });
+    }
+
+    @UsePipes(new ValidationPipe({ groups: ['list'] }))
+    @UseGuards(GqlAuthGuard)
+    @Mutation(() => Playlist)
+    addPlaylist(
+        @Args('newPlaylistInput') newPlaylistInput: NewPlaylistInput,
+        @CurrentUser() user,
+    ) {
+        return this.playlistsService.create({
+            ...newPlaylistInput,
+            user_id: user.id,
+        });
+    }
+
+    @UseGuards(GqlAuthGuard)
+    @Mutation(() => Playlist)
+    updatePlaylist(
+        @Args('updatePlaylistInput') updatePlaylistInput: UpdatePlaylistInput,
+        @Args('id') id: number,
+        @CurrentUser() user,
+    ) {
+        return this.playlistsService.update(
+            { id, user_id: user.id },
+            updatePlaylistInput,
+        );
+    }
+
+    @UseGuards(GqlAuthGuard)
+    @Mutation(() => Playlist)
+    deletePlaylist(@Args('id') id: number, @CurrentUser() user) {
+        return this.playlistsService.delete({ id, user_id: user.id });
     }
 }
